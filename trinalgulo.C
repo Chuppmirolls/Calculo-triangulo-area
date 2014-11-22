4ee856f1f50298a4d537261a071e9cd8514c7e91
Chuppmirolls <chuppmirolls@ig.com>
2014-11-22 10:55:01 -0200

    Create trinalgulo.C

---
+++ trinalgulo.C
@@ -0,0 +1,24 @@
+Calculo-triangulo-area
+======================
+
+//Calcular a área do triângulo de forma simples, apenas com númeos inteiros//
+
+#include <stdio.h>
+int main ()
+{
+  int base;
+  int altura;
+  int area;
+  
+  printf ("Digite o Valor da Base:");
+  scanf ("%i", &base);
+  
+  printf ("Digite o Valor da Altura");
+  scanf ("%i", &altura);
+  
+  area=base * altura / 2
+  
+  printf("O valor da area do triangulo e=%i",area);
+  
+  return 0;
+}  
